d361fd512f086ea4a42938894ff5055ce11c59e2
Mike <mgreen@rsgc.on.ca>
2016-02-01 09:47:52 -0500

    Tick Inside Work
    I basically just wrote what Mr. Gordon showed us on the screen.

--- Tick Challenges/Group-Maker.playground/Contents.swift
+++ Tick Challenges/Group-Maker.playground/Contents.swift
@@ -2,6 +2,10 @@
 
 import Cocoa
 
+//Specifications
+//1. Create 'n' number of groups from the list given
+//2. Groups should be randomized
+
 //Matthew Anderson
 //Stuart Armstrong
 //Puneet Bagga
@@ -27,4 +31,56 @@ import Cocoa
 //Matthew Pritchard
 //Sean Trevor
 
-var str = "Hello, playground"
+//Create data structure 
+//note: Data structures should be named using camelCasing
+var studentList = [String]()
+studentList.append("Matthew Anderson")
+studentList.append("Stuart Armstrong")
+studentList.append("Puneet Bagga")
+studentList.append("Noah Barney")
+studentList.append("Scott Blackwell")
+studentList.append("Mattias Blum")
+studentList.append("Jeremy Burleton")
+studentList.append("Liam Byrne")
+studentList.append("Gilbert Chien")
+studentList.append("Thomas Davies")
+studentList.append("Maxym Dubczak")
+studentList.append("Andrew Elder")
+studentList.append("Oliver Gane")
+studentList.append("Jeffrey Goldsmith")
+studentList.append("William Graham")
+studentList.append("Michael Green")
+studentList.append("Nicholas Jones")
+studentList.append("Brendan Leder")
+studentList.append("Alex Lo")
+studentList.append("Mark McCutcheon")
+studentList.append("Hudson McDaniel")
+studentList.append("Robert Miller")
+studentList.append("Carlos Noble Curveira")
+studentList.append("Matthew Pritchard")
+studentList.append("Sean Trevor")
+
+
+//Build a parallel array that has random value for each student
+var studentRandomPosition = [Int]()
+for i in 1...25{
+    print("\(i)")
+    studentRandomPosition.append(Int(arc4random_uniform(1000)))
+}
+
+studentRandomPosition
+
+
+//Find the highest number in the list of random numbers
+//for i in 1...25{
+//    print("The current value is: \(studentRandomPosition[i])")
+//}
+//ABOVE: An example of using a "regular loop to iterate over an array. We've illustrated a common run-time error - out of bounds condition. We ran over the end of the array.
+
+for (inex, value) in studentRandomPosition.enumerate() {
+    print("The index is \(inex) and the value is \(value)")
+}
+
+
+
+
